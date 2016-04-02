0874b42a7c202c3456781dab1f3f4af5b2861ad9
copypasteprogrammer <andrius.burtilius@ktu.edu>
2016-04-02 10:18:47 +0300

    Changed android-ndk directory

--- app/src/main/obj/local/armeabi/objs-debug/ndkLib/funkcijos.o.d
+++ app/src/main/obj/local/armeabi/objs-debug/ndkLib/funkcijos.o.d
@@ -1,5 +1,5 @@
-C:/Users/Andrius/Desktop/NDKSampleTest/app/src/main//obj/local/armeabi/objs-debug/ndkLib/funkcijos.o: \
- C:/Users/Andrius/Desktop/NDKSampleTest/app/src/main//jni/funkcijos.c \
- C:/Users/Andrius/Desktop/NDKSampleTest/app/src/main//jni/funkcijos.h
+C:/Users/Asus/Desktop/NDKSampleTest/app/src/main/obj/local/armeabi/objs-debug/ndkLib/funkcijos.o: \
+ C:/Users/Asus/Desktop/NDKSampleTest/app/src/main/jni/funkcijos.c \
+ C:/Users/Asus/Desktop/NDKSampleTest/app/src/main/jni/funkcijos.h
 
-C:/Users/Andrius/Desktop/NDKSampleTest/app/src/main//jni/funkcijos.h:
+C:/Users/Asus/Desktop/NDKSampleTest/app/src/main/jni/funkcijos.h:
